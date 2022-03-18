011a0f3f381ecb5fc45b5330180eb927358a944d
Dmitry <ryliov.work@gmail.com>
2022-03-18 16:11:03 +0100

    created individual jsx-file for todo task and import him into task-list

--- src/task-list/task-list.jsx
+++ src/task-list/task-list.jsx
@@ -1,11 +1,16 @@
 import './task-list.scss'
+import TodoTask from '../todo-task/todo-task'
 
 const TaskList = ({ isOpenState }) => {
 	return (
 		<div
 			className={isOpenState ? 'task-list-wrap' : 'task-list-wrap task-list-wrap--closed'}>
 			<ul className={isOpenState ? 'task-list' : 'task-list task-list--closed'}>
-
+				<TodoTask />
+				<TodoTask />
+				<TodoTask />
+				<TodoTask />
+				<TodoTask />
 			</ul>
 		</div>
 	)
