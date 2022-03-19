90d3da5c532bffc06840a590b4e1503e3b13e998
Dmitry <ryliov.work@gmail.com>
2022-03-19 10:13:56 +0100

    tasks dynamic creating from app

--- src/task-list/task-list.jsx
+++ src/task-list/task-list.jsx
@@ -1,16 +1,18 @@
 import './task-list.scss'
 import TodoTask from '../todo-task/todo-task'
 
-const TaskList = ({ isOpenState }) => {
+const TaskList = ({ isOpenState, tasks, completeTask }) => {
+
+	const result = tasks.map(task => {
+		const { id, ...taskData } = task
+		return <TodoTask taskData={taskData} completeTask={() => { completeTask(id) }} key={id} />
+	})
+
 	return (
 		<div
 			className={isOpenState ? 'task-list-wrap' : 'task-list-wrap task-list-wrap--closed'}>
 			<ul className={isOpenState ? 'task-list' : 'task-list task-list--closed'}>
-				<TodoTask />
-				<TodoTask />
-				<TodoTask />
-				<TodoTask />
-				<TodoTask />
+				{result}
 			</ul>
 		</div>
 	)
