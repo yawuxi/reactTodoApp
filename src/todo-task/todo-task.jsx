1c5d9f4e8c3ff4719d1712bd9015fa8d9159aa72
Dmitry <ryliov.work@gmail.com>
2022-03-19 10:16:38 +0100

    destructured data

--- src/todo-task/todo-task.jsx
+++ src/todo-task/todo-task.jsx
@@ -1,12 +1,15 @@
 import './todo-task.scss'
 
-const TodoTask = () => {
+const TodoTask = ({ taskData, completeTask }) => {
+
+	const { taskText } = taskData
+
 	return (
 		<li className="task-list__item">
 			<label>
 				<input type="checkbox" />
-				<span className='task-list__checkbox'></span>
-				<p className="task-list__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi architecto veniam deserunt neque pariatur possimus quisquam excepturi illum atque recusandae reiciendis laudantium vitae, cupiditate repellendus doloribus? Minus porro saepe quae consequatur corporis aperiam sed vel! Enim dolore placeat eos quisquam unde, debitis fugiat eius vero consequuntur eum quibusdam saepe repellendus!</p>
+				<span className='task-list__checkbox' onClick={completeTask}></span>
+				<p className="task-list__text">{taskText}</p>
 				<button className="task-list__remove">Remove</button>
 			</label>
 		</li>
